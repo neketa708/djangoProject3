7a8947f38c783d27dbe9555344dbe8872150d621
NEKETA <molnei1996@gmail.com>
2024-04-20 01:56:21 +0300

    3

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import index, about
-from .views import year_post, MonthPost
+from .views import year_post, MonthPost, ClientOrderItemsView
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('post/<int:year>/', year_post, name='year_post'),
     path('post/<int:year>/<int:month>/', MonthPost.as_view, name='mont_post'),
+    path('client_order_items/', ClientOrderItemsView.as_view(), name='client_order_items'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,9 @@
 import logging
 from django.http import HttpResponse, JsonResponse
 from django.views import View
-
+from django.utils import timezone
+from django.views.generic import TemplateView
+from django.shortcuts import render
 
 # Create your views here.
 logger = logging.getLogger(__name__)
@@ -24,3 +26,31 @@ class MonthPost(View):
         text = ""
         ...  # формируем статьи за год и месяц
         return HttpResponse(f"Posts from {month} / {year} < br > {text}")
+
+class ClientOrderItemsView(TemplateView):
+    template_name = 'myapp/client_order_items.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        client = self.request.user  # предполагается, что пользователь был аутентифицирован
+        ordered_products = []
+        orders = client.orders.filter(date__gte=timezone.now() - timezone.timedelta(days=365)).order_by('-date')
+
+        last_7_days_items = self.get_items_in_period(orders, 7, ordered_products)
+        last_30_days_items = self.get_items_in_period(orders, 30, ordered_products)
+        last_365_days_items = self.get_items_in_period(orders, 365, ordered_products)
+
+        context['last_7_days_items'] = last_7_days_items
+        context['last_30_days_items'] = last_30_days_items
+        context['last_365_days_items'] = last_365_days_items
+
+        return context
+
+    def get_items_in_period(self, orders, days, ordered_products):
+        items = []
+        for order in orders.filter(date__gte=timezone.now() - timezone.timedelta(days=days)):
+            for product in order.products.all():
+                if product not in ordered_products:
+                    items.append(product)
+                    ordered_products.append(product)
+        return items
